f9953d229c81d626050799717e6a646e17595ff1
Muhammad Anas <202210715282@mhs.ubharajaya.ac.id>
2025-05-09 15:41:48 +0700

    Create Project
    Add Library
    + Vuejs, with Prettier, Eslint
    + Bundler with Vite

---
+++ JantungIn-FE/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import HelloJantungIn from './views/HelloJantungIn.vue'
+</script>
+
+<template>
+  <HelloJantungIn />
+</template>
---
+++ JantungIn-FE/src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// Entry point for the application
+
+/*
+  Kalau mau bikin halaman baru, bikin file baru di src/views
+  Terus import di App.vue
+*/
